20d4f94c600bc050aa0e593fa4375cc0c239c433
Resurak <daniele.lopresti97@gmail.com>
2020-12-31 11:32:50 +0100

    Aggiungere i file di progetto.

---
+++ Versions/Model/Versions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace ResUtils.Versions.Model
+{
+    internal class Versions
+    {
+        [XmlAttribute]
+        public string AssemblyName { get; set; }
+
+        public Version Version { get; set; }
+    }
+
+    internal class Version
+    {
+        [XmlAttribute]
+        public DateTime BuildDate { get; set; }
+
+        public int Major { get; set; }
+        public int Minor { get; set; }
+        public int Build { get; set; }
+        public int Revision { get; set; }
+
+        public string Comment { get; set; }
+    }
+}
---
+++ Versions/VersionTracker.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace ResUtils.Versions
+{
+    public class VersionTracker
+    {
+        public VersionTracker()
+        {
+
+        }
+
+        public VersionTracker(Assembly assembly)
+        {
+            Assembly = assembly;
+        }
+
+        public VersionTracker(Assembly assembly, int major, int minor, int build, int revision)
+        {
+            Assembly = assembly;
+
+            Major = major;
+            Minor = minor;
+            Build = build;
+            Revision = revision;
+        }
+
+        public VersionTracker(Assembly assembly, int major, int minor, int build, int revision, string comment)
+        {
+            Assembly = assembly;
+
+            Major = major;
+            Minor = minor;
+            Build = build;
+            Revision = revision;
+
+            Comment = comment;
+        }
+
+
+
+        public Assembly Assembly;
+
+        public string RootFolderName;
+        public string RootFolderPath;
+
+        public string AssemblyFolderName;
+
+        public int Major, Minor, Build, Revision;
+        public string Comment = "";
+
+        internal string documents => Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+        internal string root_folderName => "ResUtils - Versions";
+        internal string rootPath => 
+            Path.Combine(
+                string.IsNullOrWhiteSpace(RootFolderPath) ? documents : RootFolderPath, 
+                string.IsNullOrWhiteSpace(RootFolderName) ? root_folderName : RootFolderName
+                );
+
+        internal void Save()
+        {
+            if (Assembly != null)
+            {
+                Directory.CreateDirectory(rootPath);
+
+                AssemblyFolderName = Assembly.FullName;
+            }
+        }
+
+        internal void Load()
+        {
+
+        }
+    }
+}
